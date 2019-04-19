1bece0961262de87260a63351078edf5bc0a7c3b
Sean von Bayern <seanvonbayern@gmail.com>
2019-04-18 22:30:00 -0700

    Add new Mapbox API tokens to main.js and restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoic2VhbnZvbmIiLCJhIjoiY2p1bm0wdnZjMDJ1NjN5cnZidWJxdzBvbSJ9.52AlhHseM7nwsY0k7yVz2g',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
