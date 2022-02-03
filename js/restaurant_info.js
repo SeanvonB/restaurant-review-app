0c768c2ac290db04146b67fa1990b551e3763ae3
SeanvonB <42122062+SeanvonB@users.noreply.github.com>
2022-02-02 18:15:21 -0800

    Enable service worker

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -235,11 +235,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	initMap();
 
 	// Register service worker
-	// if ("serviceWorker" in navigator) {
-	//   navigator.serviceWorker
-	//     .register("/sw.js")
-	//     .catch(function(error){
-	//       console.error(error);
-	//     });
-	// }
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker.register("/sw.js").catch(function (error) {
+			console.error(error);
+		});
+	}
 });
